ff1460c5a4cf51694536ad4ea50fa60252ddf668
MrBeeBenson <90976178+MrBeeBenson@users.noreply.github.com>
2021-10-26 19:59:54 +0100

    Update and rename mrbeearch.sh to script1.sh

---
+++ script1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dev='/dev/vda'
+umount "$dev"
+printf "o\nn\np\n1\n\n\nw\n" | fdisk "$dev"
+mkfs.ext4 "${dev}1"
+
+pacman -Syy
+
+mount /dev/vda1 /mnt
+
+pacstrap /mnt base linux linux-firmware nano
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
